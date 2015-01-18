f83498986933fb639079ac6bcfe011f0af283574
joolsmf <joolsmf@gmail.com>
2015-01-18 13:08:32 +0000

    tweaked code

--- cw/moveTest2.pl
+++ cw/moveTest2.pl
@@ -14,33 +14,26 @@ barrier(4, 9).
 barrier(5, 2).
 
 
-/*
-barrier base case
-movedown([A,B], _, [[A1,B]]) :-
-	barrier(A,B),
-	A1 is A - 1,
-	!.
-*/
-
 /* move along y coordinates */
 
-
-
+%has destination been reached?
 movedown([A,B],[A,_],[[A,B]]) :- !.
-
+% look ahead to see if barrier is ahead
 movedown([A,B],_,[[A,B]]) :- Y is A + 1, barrier(Y,B),!.
-
 
 movedown( [Y,X], [EndY,_], [[Y,X] | Result] ) :-
 		Y < EndY,
 		CurrentY is Y + 1,
 		movedown([CurrentY,X], [EndY,_],Result).
 
+
+
 moveup([A,B],[A,_],[[A,B]]) :- !.
 moveup( [Y,X], [EndY,_], [[Y,X] | Result] ) :-
 		Y > EndY,
 		CurrentY is Y - 1,
 		moveup([CurrentY,X], [EndY,_],Result).
+
 
 /* move along x coordinates*/
 
