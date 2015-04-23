5d9fbd7b5a7727efb9e211be30c2b9f958571a3e
joolsmf <joolsmf@gmail.com>
2015-04-23 22:57:43 +0100

    finished and bug free now

--- maze-solver.pl
+++ maze-solver.pl
@@ -1,31 +1,4 @@
-/*
-NOTE ON BUG
-
-Semi-working. However getting "false" for some things eg
-it can get to
-      solve((1,1),(1,9),P)
-but not
-      solve((1,1),(1,9),P)
-
-'Robot' not moving back on itself in certain scenarios. Problem occurs
-if start and end X or Y coordinates are the same and there is a barrier
-between them such that the robot has to move in a U shape.
-
-?- solve((1,1),(2,9),P).
-  1 2 3 4 5 6 7 8 9
-  +-----------------+
-1|* * * * * * . X . |
-2|X X . X X * * * * |
-3|. . . X . . X . X |
-4|. . . X . . X X X |
-5|. X . . . . . . . |
-  +-----------------+
-
-?- solve((1,1),(1,9),P).
-false.
-
-*/
-
+/* MAZE FACTS */
 
 mazeSize(5, 9).
 barrier(1, 8).
@@ -42,8 +15,6 @@ barrier(4, 8).
 barrier(4, 9).
 barrier(5, 2).
 
-
-
 insidemaze(Y,X) :-
 	mazeSize(Y1,X1),
 	\+ Y > Y1,
@@ -51,56 +22,50 @@ insidemaze(Y,X) :-
 	\+ X > X1,
 	\+ X < 1.
 
-
-
-findValidMove((Y, X),(YTo,_),(NextY,NextX)) :-
-	Y < YTo,
-	moveDown((Y,X),(NextY,NextX)),
-	\+ barrier(NextY,NextX),
-	insidemaze(NextY,NextX).
-
-findValidMove((Y, X),(YTo,_),(NextY,NextX)) :-
-	Y > YTo,
-	moveUp((Y,X),(NextY,NextX)),
-	\+ barrier(NextY,NextX),
-	insidemaze(NextY,NextX).
-
-findValidMove((Y, X),(_,XTo),(NextY,NextX)) :-
-	X < XTo,
-	moveRight((Y,X),(NextY,NextX)),
-	\+ barrier(NextY,NextX),
-	insidemaze(NextY,NextX).
-
-findValidMove((Y, X),(_,XTo),(NextY,NextX)) :-
-	X > XTo,
-	moveLeft((Y,X),(NextY,NextX)),
-	\+ barrier(NextY,NextX),
-	insidemaze(NextY,NextX).
-
-
-
-
-moveDown((Y,X),(NextY,X)) :-
-	NextY is Y + 1.
-moveUp((Y,X),(NextY,X)) :-
-	NextY is Y - 1.
-moveRight((Y,X),(Y, NextX)) :-
-	NextX is X + 1.
-moveLeft((Y,X),(Y, NextX)) :-
-	NextX is X - 1.
-
-
-
+/* MAIN PREDICATE */
 
 solve(From,To,Path) :-
-	findPath(From,To,Path),
+	findPath(From,To,[],Path),
 	printMaze(Path), !.
 
-findPath(To,To,[To]) :- !.
-findPath(From, To, [From|Result] ) :-
-  findValidMove(From,To, FromNext),
-  findPath(FromNext,To,Result).
+findPath(To,To,_,[To]) :- !.
+findPath(From, To, ListSoFar,[From|Result] ) :-
+  findValidMove(From,FromNext),
+  /* Crucial test, check whether coordinate has already been visited*/
+  \+ member(FromNext,ListSoFar),
+  findPath(FromNext,To,[FromNext|ListSoFar],Result).
 
+
+/* MAKE A MOVE */
+findValidMove([Y, X],[NextY,NextX]) :-
+	moveDown([Y,X],[NextY,NextX]),
+	\+ barrier(NextY,NextX),
+	insidemaze(NextY,NextX).
+
+findValidMove([Y, X],[NextY,NextX]) :-
+	moveUp([Y,X],[NextY,NextX]),
+	\+ barrier(NextY,NextX),
+	insidemaze(NextY,NextX).
+
+findValidMove([Y, X],[NextY,NextX]) :-
+	moveRight([Y,X],[NextY,NextX]),
+	\+ barrier(NextY,NextX),
+	insidemaze(NextY,NextX).
+
+findValidMove([Y, X],[NextY,NextX]) :-
+	moveLeft([Y,X],[NextY,NextX]),
+	\+ barrier(NextY,NextX),
+	insidemaze(NextY,NextX).
+
+
+moveDown([Y,X],[NextY,X]) :-
+	NextY is Y + 1.
+moveUp([Y,X],[NextY,X]) :-
+	NextY is Y - 1.
+moveRight([Y,X],[Y, NextX]) :-
+	NextX is X + 1.
+moveLeft([Y,X],[Y, NextX]) :-
+	NextX is X - 1.
 
 
 
@@ -119,7 +84,6 @@ printBottomLine :-
 	write('  +-----------------+').
 
 
-
 printRows(Row,_,_) :-
 	Row = 6,
 	!.
@@ -132,7 +96,6 @@ printRows(Row,Col,Path) :-
 	printRows(NextRow,Col,Path).
 
 
-
 printLine(_,Col,_) :-
 	Col = 10, !.
 printLine(Row,Col,Path) :-
@@ -141,9 +104,8 @@ printLine(Row,Col,Path) :-
 	printLine(Row,NextCol,Path).
 
 
-
 printElement(Row,Col,Path) :-
-	member((Row,Col),Path),
+	member([Row,Col],Path),
 	write('* ').
 printElement(Row,Col,_) :-
 	barrier(Row,Col),
